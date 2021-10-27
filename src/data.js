420764745a3f4aafb8af13c9cf31ca5a124fc8fe
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-10-28 00:19:47 +0100

    Create a function that iterate over the array of objects and populate them to the browser

---
+++ src/data.js
@@ -0,0 +1,19 @@
+const tasks = [
+  {
+    desc: 'Solve coding challenge',
+    completed: false,
+    index: 0,
+  },
+  {
+    desc: 'Attend tech conference',
+    completed: false,
+    index: 1,
+  },
+  {
+    desc: 'Go to gym',
+    completed: false,
+    index: 2,
+  },
+];
+
+export default tasks;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
 import './style.css';
+import tasks from './data';
 
+const output = document.querySelector('.output');
+const ul = document.createElement('ul');
+ul.classList.add('list-group', 'list-unstyled');
+
+const createTask = ({ desc, completed, index }) => { // eslint-disable-line no-unused-vars
+  const li = document.createElement('li');
+  li.className = 'list-group-items';
+  const div = document.createElement('div');
+  div.classList.add('d-flex', 'justify-content-between', 'align-items-center', 'px-2', 'py-3', 'border', 'border-top');
+  const label = document.createElement('label');
+  label.innerHTML = `<input type="checkbox" id="checkbox"> &nbsp; &nbsp; ${desc}`;
+
+  const verticalDots = document.createElement('i');
+  verticalDots.classList.add('bi', 'bi-three-dots-vertical');
+  verticalDots.style.cursor = 'pointer';
+
+  div.append(label, verticalDots);
+  li.appendChild(div);
+  ul.appendChild(li);
+  output.appendChild(ul);
+};
+
+const displayTasks = () => {
+  tasks.forEach(createTask);
+};
+
+displayTasks();
