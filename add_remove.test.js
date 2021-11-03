77f86935eb352549560445d047135fbae6d09966
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-11-03 22:15:38 +0100

    Try to test for removeTodo function, but with no lno luck

--- add_remove.test.js
+++ add_remove.test.js
@@ -1,14 +1,25 @@
+import { addTodo, removeTodo } from './src/functions';
 
-/*const addTodo = require('./src/functions');*/
-import {addTodo} from './src/functions.js';
 describe('test add task function', () => {
-  let taskList = [];
-  test('test add new items in task list array',() => {
+  const taskList = [];
+  test('test add new item in task list array', () => {
     addTodo('do my daily workout', taskList);
     expect(taskList).toHaveLength(1);
   });
-  test('give the right index for the new item',() => {
+  test('give the right index for the new item', () => {
     addTodo('do my daily workout', taskList);
     expect(taskList[taskList.length - 1].index).toBe(2);
   });
 });
+
+/*
+describe('test to remove task function', () => {
+  const taskList = [];
+  const taskListLen = taskList.length;
+  const text = 'text';
+  test('should remove item from the list array', () => {
+    removeTodo(text, taskList);
+    expect(taskList).toHaveLength(taskListLen - 1);
+  });
+});
+*/
--- src/functions.js
+++ src/functions.js
@@ -38,4 +38,3 @@ export {
   removeCompleted,
   updateIndex,
 };
-/*module.exports = addTodo;*/
